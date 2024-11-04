233a4f452e42a0c0ce0b203d24431d365c35c0b2
PedroXL23 <2307pedro.silva@gmail.com>
2024-11-04 15:25:49 -0300

    Add files via upload

---
+++ app/app/page.tsx
@@ -0,0 +1,25 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <header className="font-bold text-xl mb-4"> 
+        <h3 className="font-bold text-xl mb-4 flex justify-center items-center text-center  " >Somos a Bezzs, uma plataforma criada para conectar pessoas que precisam se locomover com motoristas que oferecem esse serviço de maneira confiável e personalizada. Nosso principal objetivo é facilitar a mobilidade urbana, proporcionando conforto, praticidade e rapidez. Somos mais do que uma plataforma de transporte: somos parceiros da cidade e buscamos melhorar a vida das pessoas por meio de soluções inovadoras</h3>
+      </header>
+      <div className="h-screen flex justify-center items-center bg-slate-600 px-5"> 
+      <form className="bg-white p-12 rounded-lg w-96 max-w-full flex justify center items justify-center flex-col gap-2">
+        <h2 className="font-bold text-xl mb-4 flex justify-center items-center ">Login</h2>
+        <input type="email" 
+        placeholder="Usuário"
+        className="input input-primary w-full" />
+        <input type="password" 
+        placeholder="Senha"
+        className="input input-primary w-full"
+        />
+        <button className="btn btn-primary w-full "> Acessar </button>
+      </form>
+       </div>
+    </main>
+    
+  );
+}
